2de873117553b666ca4cebb8aca2be485fd97509
刘振宇 <liuzhenyulive@live.com>
2018-01-20 15:42:46 +0800

    命令行配置

---
+++ 2-配置管理-2.1命令行配置/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace _2_配置管理_2._1命令行配置
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var  settings=new Dictionary<string,string>()
+          {
+              { "name","123"},
+              { "age","100"}
+          };
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(settings)
+                .AddCommandLine(args);
+            var configuration = builder.Build();
+            Console.WriteLine($"name:{configuration["name"]}");
+            Console.WriteLine($"age:{configuration["age"]}");
+            Console.ReadLine();
+        }
+    }
+}
